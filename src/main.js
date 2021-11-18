ecd3292916f170a4a54ad1148757419b84b192af
Anani <aananiev@gmail.com>
2021-11-19 00:33:28 +0200

    Create basic component structure

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 
-createApp(App).mount("#app");
+import Dashboard from "./Dashboard.vue";
+import { createStore } from "vuex";
+
+createApp(Dashboard)
+  .use(
+    createStore({
+      state() {
+        return {
+          ratePairs: [
+            { id: 1, codeFrom: "HIVE", codeTo: "USD" },
+            { id: 2, codeFrom: "Starbits", codeTo: "SWAP.HIVE" },
+            { id: 3, codeFrom: "Sim", codeTo: "SWAP.HIVE" },
+          ],
+        };
+      },
+    })
+  )
+  .mount("#app");
